ff908a3195fad97595533533b450969e2db6d70c
Taha Al Akab <alakabtaha@gmail.com>
2024-02-07 22:46:46 +0000

    Finishing up projects section

---
+++ src/components/Cards.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import '../styles/cards.css';
+
+const Cards = ({ link, imageSrc, title, description, dateCompleted, techStack }) => {
+    return (
+        <a className='box-link' href={link} target='_blank'>
+            <div className='card-format'>
+                <div className='card-img'>
+                    <img src={imageSrc} alt='templ' />
+                </div>
+                <div className='card-body'>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <p>Date {dateCompleted}</p>
+                    <p>Tech Stack</p>
+                        <div className='tech-stack-format'>
+                            {
+                                techStack.map((tech, index) => {
+                                    return (
+                                        <div key={index} className='tech-stack-child'><p>{tech}</p></div>
+                                    )
+                                })
+                            }
+                        </div>
+                        {/* {
+                        techStack.map((tech, index) => {
+                            return (
+                                <span key={index}>{tech}</span>
+                            )
+                        }) */}
+                   
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default Cards
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,25 +1,56 @@
 import React from 'react'
 import '../styles/projects.css';
 import GoogleScraper from './../assets/google-scraper.png';
+import Cards from '../components/Cards';
 
 const Projects = () => {
+
+    const projectData = [
+        {
+            link: 'https://www.google.com',
+            imageSrc: GoogleScraper,
+            title: 'Barcode Scraper',
+            description: 'A web scraper that uses puppeteer to scrape google search results for barcode numbers',
+            dateCompleted: '06/02/24',
+            techStack: ['Python', 'Node.js', 'React', 'MongoDB', 'Express.js']
+        },
+        {
+            link: 'https://www.google.com',
+            imageSrc: GoogleScraper,
+            title: 'Barcode Google',
+            description: 'A web scraper that uses puppeteer to scrape google search results for barcode numbers',
+            dateCompleted: '06/02/24',
+            techStack: ['Python']
+        },
+        {
+            link: 'https://www.google.com',
+            imageSrc: GoogleScraper,
+            title: 'Google Scraper',
+            description: 'A web scraper that uses puppeteer to scrape google search results for barcode numbers',
+            dateCompleted: '06/02/24',
+            techStack: ['Python', 'Puppeteer']
+        }
+    ]
+
     return (
         <section id='projects'>
             <h1 className='sf-font'>Projects</h1>
             <div className='card-carousel'>
-                <div className='card-format'>
-                    <div className='card-img'>
-                        <img src={GoogleScraper} alt='templ' />
-                    </div>
-                    <div className='card-body'>
-                        <h2>Google bar code automation</h2>
-                        <p>A software to automatically query google and find out what item is associated with the barcode and store it in a database</p>
-                        <p>Date: 01/01/19</p>
-                        <p>Tech Stack<br /><span>Python</span><span>JavaScript</span></p>
-                    </div>
-                </div>
-                <div className='card-format'></div>
-                <div className='card-format'></div>
+                {
+                    projectData.map((project, index) => {
+                        return (
+                            <Cards 
+                                key={index}
+                                link={project.link}
+                                imageSrc={project.imageSrc}
+                                title={project.title}
+                                description={project.description}
+                                dateCompleted={project.dateCompleted}
+                                techStack={project.techStack}
+                            />
+                        )
+                    })
+                }
             </div>
         </section>
     )
